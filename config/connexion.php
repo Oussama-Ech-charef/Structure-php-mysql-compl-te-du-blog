32f549eb8433fe11b27690bd08e7e9204dea339c
OUSSAMA <oussamacharef71@gmail.com>
2026-04-25 23:34:13 +0100

    Add database connection configuration

---
+++ config/connexion.php
@@ -0,0 +1,29 @@
+<?php
+
+class Database {
+
+
+
+        private $host = "localhost";
+        private $db_name = "tangier_blog";
+        private $username = "root";
+        private $password = "";
+        public $conn;
+
+
+        public function getConnection() {
+            $this->conn = null;
+
+            try {
+                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name};charset=utf8", $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            }catch(PDOException $e) {
+                echo "Database connection error." . $e->getMessage();
+            }
+            return $this->conn;
+
+
+
+        }
+}
